942d960f1fc4cec850b1f3603b0b3b0da3a434ee
Anisa Foroozan <aforooza@uwo.ca>
2025-03-14 10:38:57 -0400

    comments

--- viewAlarm.cpp
+++ viewAlarm.cpp
@@ -1,32 +1,70 @@
+/**
+ * @file viewAlarm.cpp
+ * @brief Implementation of the ViewAlarm class.
+ * 
+ * This file implements the ViewAlarm class, which provides a GUI for viewing 
+ * a list of active alarms. The class allows users to see alarms along with 
+ * their labels and close the window when needed.
+ * 
+ * The widget consists of:
+ * - A QLabel for the title.
+ * - A QListWidget to display the alarms.
+ * - A QPushButton to close the window.
+ * 
+ * @author Group 27
+ * @date Friday, March 14
+ */
 #include "viewAlarm.h"
 
+/**
+ * @brief Constructs a ViewAlarm window.
+ * 
+ * @details Initializes the alarm viewing window with a title, a list widget
+ * to display alarms, and a close button.
+ * 
+ * @param[in] parent Pointer to the parent widget. Defaults to nullptr.
+ */
+
 ViewAlarm::ViewAlarm(QWidget *parent) : QWidget(parent) {
-    setWindowTitle("View Alarms");
+    setWindowTitle("View Alarms"); /**< Sets the window title to "View Alarms". */
     setWindowFlags(Qt::Window);
 
-    QVBoxLayout *layout = new QVBoxLayout(this);
+    QVBoxLayout *layout = new QVBoxLayout(this); /**< Creates a vertical layout for the window. */
 
-    QLabel *titleLabel = new QLabel("Active Alarms:", this);
+    QLabel *titleLabel = new QLabel("Active Alarms:", this); /**< Creates a label displaying "Active Alarms:". */
     layout->addWidget(titleLabel);
 
-    // List widget to display alarms
-    alarmListWidget = new QListWidget(this);
+   
+    alarmListWidget = new QListWidget(this); /**< Initializes the QListWidget to display alarms. */
     layout->addWidget(alarmListWidget);
 
-    QPushButton *closeButton = new QPushButton("Close", this);
+    QPushButton *closeButton = new QPushButton("Close", this); /**< Creates a "Close" button. */
     layout->addWidget(closeButton);
 
+    // Connect close button to the close() function of QWidget
     connect(closeButton, &QPushButton::clicked, this, &QWidget::close);
 
     setLayout(layout);
 }
 
-// Method to update alarm list
+/**
+ * @brief Updates the displayed alarm list.
+ * 
+ * @details Clears the existing alarm list and repopulates it with new alarms 
+ * and their associated labels.
+ * 
+ * @param[in] alarms A QList of QTime objects representing alarm times.
+ * @param[in] labels A QList of QString objects representing alarm labels.
+ */
+
 void ViewAlarm::updateAlarmList(const QList<QTime> &alarms, const QList<QString> &labels) {
-    alarmListWidget->clear(); // Clear old data
+
+    alarmListWidget->clear(); /**< Clears the existing alarm list to avoid duplicates. */
 
     for (int i = 0; i < alarms.size(); ++i) {
-        QString alarmText = labels[i] + " - " + alarms[i].toString("HH:mm");
+
+        QString alarmText = labels[i] + " - " + alarms[i].toString("HH:mm"); /**< Formats the alarm display text. */
+
         alarmListWidget->addItem(alarmText);
     }
 }
--- viewAlarm.h
+++ viewAlarm.h
@@ -1,3 +1,18 @@
+/**
+ * @file viewAlarm.h
+ * @brief Definition of the ViewAlarm class.
+ * 
+ * This file defines the ViewAlarm class, which provides a graphical interface 
+ * for displaying a list of active alarms with their associated labels.
+ * 
+ * The widget consists of:
+ * - A QLabel for the title.
+ * - A QListWidget to display the alarms.
+ * - A QPushButton to close the window.
+ * 
+ * @author Group 27
+ * @date Friday, March 14
+ */
 #ifndef VIEWALARM_H
 #define VIEWALARM_H
 
@@ -8,15 +23,39 @@
 #include <QListWidget>
 #include <QTime>
 
+/**
+ * @brief The ViewAlarm class provides a GUI for displaying active alarms.
+ * 
+ * @details This class extends QWidget and presents a list of active alarms in a QListWidget.
+ * Users can view alarms with associated labels and close the window when needed.
+ */
+
 class ViewAlarm : public QWidget {
     Q_OBJECT
 
 public:
+    /**
+     * @brief Constructs a ViewAlarm widget.
+     * 
+     * @param[in] parent Pointer to the parent widget. Defaults to nullptr.
+     */
+
     explicit ViewAlarm(QWidget *parent = nullptr);
+
+    /**
+     * @brief Updates the alarm list displayed in the widget.
+     * 
+     * @details This function clears the existing list and populates it with 
+     * new alarms and their labels.
+     * 
+     * @param[in] alarms A list of QTime objects representing alarm times.
+     * @param[in] labels A list of QString objects representing alarm labels.
+     */
+
     void updateAlarmList(const QList<QTime> &alarms, const QList<QString> &labels); // New method
 
 private:
-    QListWidget *alarmListWidget;
+    QListWidget *alarmListWidget; /**< Widget to display the list of alarms. */
 };
 
 #endif // VIEWALARM_H
