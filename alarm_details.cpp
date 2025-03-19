1d57e7e253ba9d18d06082d2784f1bfb3901821e
Changyi Huo <chuo5@uwo.ca>
2025-03-19 16:54:27 -0400

    modifying an existing alarm

--- alarm_details.cpp
+++ alarm_details.cpp
@@ -2,38 +2,84 @@
 
 /**
  * @brief Constructs the AlarmDetails window.
- * @param alarmLabel The label of the alarm to display.
+ * @param time The alarm's time.
+ * @param repeat The alarm's repeat setting.
+ * @param label The alarm's label.
+ * @param sound The alarm's sound setting.
  */
-AlarmDetails::AlarmDetails(QString alarmLabel, QWidget *parent)
+AlarmDetails::AlarmDetails(QTime time, QString repeat, QString label, QString sound, QWidget *parent)
     : QDialog(parent) {
-    setWindowTitle("Alarm Details");
-    setFixedSize(250, 150);
+    setWindowTitle("Modify Alarm");
 
-    QVBoxLayout *mainLayout = new QVBoxLayout(this);
+    QVBoxLayout *layout = new QVBoxLayout(this);
 
-    alarmLabelDisplay = new QLabel("Alarm: " + alarmLabel, this);
-    mainLayout->addWidget(alarmLabelDisplay);
+    // Time Selection
+    layout->addWidget(new QLabel("Change Time:"));
+    timeEdit = new QTimeEdit(time, this);
+    timeEdit->setDisplayFormat("HH:mm");
+    layout->addWidget(timeEdit);
 
-    QHBoxLayout *buttonLayout = new QHBoxLayout();
-    
-    deleteButton = new QPushButton("Delete", this);
+    // Repeat Dropdown
+    layout->addWidget(new QLabel("Repeat:"));
+    repeatComboBox = new QComboBox(this);
+    repeatComboBox->addItem("Never");
+    repeatComboBox->addItem("Every Sunday");
+    repeatComboBox->addItem("Every Monday");
+    repeatComboBox->addItem("Every Tuesday");
+    repeatComboBox->addItem("Every Wednesday");
+    repeatComboBox->addItem("Every Thursday");
+    repeatComboBox->addItem("Every Friday");
+    repeatComboBox->addItem("Every Saturday");
+    repeatComboBox->setCurrentText(repeat);
+    layout->addWidget(repeatComboBox);
+
+    // Label Input
+    layout->addWidget(new QLabel("Change Label:"));
+    labelEdit = new QLineEdit(label, this);
+    layout->addWidget(labelEdit);
+
+    // Sound Dropdown
+    layout->addWidget(new QLabel("Sound:"));
+    soundComboBox = new QComboBox(this);
+    soundComboBox->addItem("Default");
+    soundComboBox->addItem("Beep");
+    soundComboBox->addItem("Chime");
+    soundComboBox->addItem("Birdsong");
+    soundComboBox->addItem("Classic Alarm");
+    soundComboBox->setCurrentText(sound);
+    layout->addWidget(soundComboBox);
+
+    // Buttons
+    modifyButton = new QPushButton("Modify Alarm", this);
+    deleteButton = new QPushButton("Delete Alarm", this);
     closeButton = new QPushButton("Close", this);
 
+    QHBoxLayout *buttonLayout = new QHBoxLayout();
+    buttonLayout->addWidget(modifyButton);
     buttonLayout->addWidget(deleteButton);
     buttonLayout->addWidget(closeButton);
 
-    mainLayout->addLayout(buttonLayout);
+    layout->addLayout(buttonLayout);
 
-    connect(closeButton, &QPushButton::clicked, this, &QDialog::close);
+    connect(modifyButton, &QPushButton::clicked, this, &AlarmDetails::modifyAlarm);
     connect(deleteButton, &QPushButton::clicked, this, &AlarmDetails::onDeleteClicked);
+    connect(closeButton, &QPushButton::clicked, this, &QDialog::close);
 
-    setLayout(mainLayout);
+    setLayout(layout);
+}
+
+/**
+ * @brief Handles the modification of the alarm.
+ */
+void AlarmDetails::modifyAlarm() {
+    emit alarmModified(timeEdit->time(), repeatComboBox->currentText(), labelEdit->text(), soundComboBox->currentText());
+    close(); // Close the dialog
 }
 
 /**
  * @brief Handles the deletion of the alarm.
  */
 void AlarmDetails::onDeleteClicked() {
-    emit alarmDeleted(alarmLabelDisplay->text().remove("Alarm: ")); // Emit signal with alarm label
+    emit alarmDeleted(labelEdit->text()); // Emit signal with alarm label
     close(); // Close the dialog
 }
--- moc_alarm_details.cpp
+++ moc_alarm_details.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_AlarmDetails_t {
-    QByteArrayData data[5];
-    char stringdata0[54];
+    QByteArrayData data[11];
+    char stringdata0[104];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -33,14 +33,21 @@ struct qt_meta_stringdata_AlarmDetails_t {
 static const qt_meta_stringdata_AlarmDetails_t qt_meta_stringdata_AlarmDetails = {
     {
 QT_MOC_LITERAL(0, 0, 12), // "AlarmDetails"
-QT_MOC_LITERAL(1, 13, 12), // "alarmDeleted"
-QT_MOC_LITERAL(2, 26, 0), // ""
-QT_MOC_LITERAL(3, 27, 10), // "alarmLabel"
-QT_MOC_LITERAL(4, 38, 15) // "onDeleteClicked"
+QT_MOC_LITERAL(1, 13, 13), // "alarmModified"
+QT_MOC_LITERAL(2, 27, 0), // ""
+QT_MOC_LITERAL(3, 28, 4), // "time"
+QT_MOC_LITERAL(4, 33, 6), // "repeat"
+QT_MOC_LITERAL(5, 40, 5), // "label"
+QT_MOC_LITERAL(6, 46, 5), // "sound"
+QT_MOC_LITERAL(7, 52, 12), // "alarmDeleted"
+QT_MOC_LITERAL(8, 65, 10), // "alarmLabel"
+QT_MOC_LITERAL(9, 76, 11), // "modifyAlarm"
+QT_MOC_LITERAL(10, 88, 15) // "onDeleteClicked"
 
     },
-    "AlarmDetails\0alarmDeleted\0\0alarmLabel\0"
-    "onDeleteClicked"
+    "AlarmDetails\0alarmModified\0\0time\0"
+    "repeat\0label\0sound\0alarmDeleted\0"
+    "alarmLabel\0modifyAlarm\0onDeleteClicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -50,23 +57,27 @@ static const uint qt_meta_data_AlarmDetails[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       2,   14, // methods
+       4,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
-       1,       // signalCount
+       2,       // signalCount
 
  // signals: name, argc, parameters, tag, flags
-       1,    1,   24,    2, 0x06 /* Public */,
+       1,    4,   34,    2, 0x06 /* Public */,
+       7,    1,   43,    2, 0x06 /* Public */,
 
  // slots: name, argc, parameters, tag, flags
-       4,    0,   27,    2, 0x08 /* Private */,
+       9,    0,   46,    2, 0x08 /* Private */,
+      10,    0,   47,    2, 0x08 /* Private */,
 
  // signals: parameters
-    QMetaType::Void, QMetaType::QString,    3,
+    QMetaType::Void, QMetaType::QTime, QMetaType::QString, QMetaType::QString, QMetaType::QString,    3,    4,    5,    6,
+    QMetaType::Void, QMetaType::QString,    8,
 
  // slots: parameters
+    QMetaType::Void,
     QMetaType::Void,
 
        0        // eod
@@ -78,16 +89,25 @@ void AlarmDetails::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id
         auto *_t = static_cast<AlarmDetails *>(_o);
         (void)_t;
         switch (_id) {
-        case 0: _t->alarmDeleted((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 1: _t->onDeleteClicked(); break;
+        case 0: _t->alarmModified((*reinterpret_cast< QTime(*)>(_a[1])),(*reinterpret_cast< QString(*)>(_a[2])),(*reinterpret_cast< QString(*)>(_a[3])),(*reinterpret_cast< QString(*)>(_a[4]))); break;
+        case 1: _t->alarmDeleted((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 2: _t->modifyAlarm(); break;
+        case 3: _t->onDeleteClicked(); break;
         default: ;
         }
     } else if (_c == QMetaObject::IndexOfMethod) {
         int *result = reinterpret_cast<int *>(_a[0]);
         {
+            using _t = void (AlarmDetails::*)(QTime , QString , QString , QString );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&AlarmDetails::alarmModified)) {
+                *result = 0;
+                return;
+            }
+        }
+        {
             using _t = void (AlarmDetails::*)(const QString & );
             if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&AlarmDetails::alarmDeleted)) {
-                *result = 0;
+                *result = 1;
                 return;
             }
         }
@@ -123,22 +143,29 @@ int AlarmDetails::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 2)
+        if (_id < 4)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 2;
+        _id -= 4;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 2)
+        if (_id < 4)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 2;
+        _id -= 4;
     }
     return _id;
 }
 
 // SIGNAL 0
+void AlarmDetails::alarmModified(QTime _t1, QString _t2, QString _t3, QString _t4)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))), const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t2))), const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t3))), const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t4))) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+
+// SIGNAL 1
 void AlarmDetails::alarmDeleted(const QString & _t1)
 {
     void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
-    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+    QMetaObject::activate(this, &staticMetaObject, 1, _a);
 }
 QT_WARNING_POP
 QT_END_MOC_NAMESPACE
